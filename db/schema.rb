57ae0c421208450784ac8330478ccacc70106ea8
Richard Slack <slack.dick@gmail.com>
2018-08-18 12:19:47 +0700

    Stripe Gem and form added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180817023425) do
+ActiveRecord::Schema.define(version: 20180818040447) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

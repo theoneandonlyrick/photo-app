445f246a2eacbf81bd1f77faad0c26bbd1d1a1e6
Richard Slack <slack.dick@gmail.com>
2018-08-19 16:13:51 +0700

    fixing stripe

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -14,6 +14,6 @@ class Payment < ApplicationRecord
     customer = Stripe::Customer.create email: email, card: token
 
     Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'usd'
-    end
+    
   end
 end
